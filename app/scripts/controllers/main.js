762e5025f824f9aaad9a1c0d9880d391798daeb5
Eduardo Molinar <edy.0440@gmail.com>
2016-11-27 18:25:25 -0500

    primer commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyecto2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the proyecto2App
+ */
+angular.module('proyecto2App')
+  .controller('MainCtrl', function () {
+
+  });
